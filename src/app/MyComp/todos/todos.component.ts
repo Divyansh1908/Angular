8b1c5820f463edd623215134765415b35cdac7ce
Divyansh Singh <divyansh301@gmail.com>
2024-04-01 17:43:41 -0500

    Generating a model and populating and displaying it using directives

--- src/app/MyComp/todos/todos.component.ts
+++ src/app/MyComp/todos/todos.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { todo } from '../../todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
-export class TodosComponent {
 
+export class TodosComponent implements OnInit {
+  todos: todo[];
+
+  constructor(){
+    this.todos = [
+      {
+        sno:3,
+        title:"this is ttle",
+        desc:"description",
+        active:true
+      },
+      {
+        sno:4,
+        title:"this is ttle2",
+        desc:"description",
+        active:true
+      },
+      {
+        sno:5,
+        title:"this is ttle3",
+        desc:"description",
+        active:true
+      },
+      {
+        sno:6,
+        title:"this is ttle4",
+        desc:"description",
+        active:true
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,8 @@
+/*Has the todo models that will have all the todos that we make*/
+
+export class todo{
+    sno: number
+    title: string
+    desc: string
+    active: boolean
+}
